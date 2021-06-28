d64bce70703206adcdbf8ab557585ab0ba3022ba
Parth Raut <parthlr321@gmail.com>
2021-06-28 12:33:20 -0400

    New profiles can dynamically add/remove LEDs evenly

--- rgb/static/led_creator.js
+++ rgb/static/led_creator.js
@@ -1,30 +1,38 @@
-var baseNumber = 10;
+var previousNumber = 10;
+$('#leds').append("<div class='flex-container' id='flex10'></div>");
+var led_group = "";
 
 $('#num_leds').on('input', function(e) {
   var leds = $('#num_leds').val();
-  var led_group = "<div class='flex-container'>";
-  for (var i = 0; i < leds; i++) {
-    if (i % 10 == 0 && i != 0) {
-      led_group += "</div>";
-      $('#leds').append(led_group);
-      led_group = "";
-      led_group += "<div class='flex-container'>";
-    }
-    led_group += "<div id='led" + (i + 10) + "' class='input-group' title='Using input value'>" +
-                    "<input type='text' name='led" + (i + 10) + "' class='form-control input-lg' value='#000000' hidden>" +
-                    "<span class='input-group-append'>" +
-                      "<span class='input-group-text colorpicker-input-addon'><i></i></span>" +
-                    "</span>" +
-                  "</div>" +
-                  "<script>" +
-                    "$(function () {" +
-                      "$('#led" + (i + 10) + "').colorpicker({" +
-                        "format: 'hex'" +
-                      "});" +
-                    "});" +
-                  "</script>";
+  if (leds == '') {
+    leds = 0;
   }
-  //led_group += "</div>";
-  //$('#leds').append(led_group);
-  //baseNumber += leds;
+  if (leds > previousNumber) {
+    for (var i = previousNumber; i < leds; i++) {
+      if (i % 10 == 0) {
+          $('#leds').append("<div class='flex-container' id='flex" + i.toString() + "'></div>");
+      }
+      led_group = "<div id='led" + i.toString() + "' class='input-group' title='Using input value'>" +
+                      "<input type='text' name='led" + i.toString() + "' class='form-control input-lg' value='#000000' hidden>" +
+                      "<span class='input-group-append'>" +
+                        "<span class='input-group-text colorpicker-input-addon'><i></i></span>" +
+                      "</span>" +
+                    "</div>" +
+                    "<script id='colorpicker" + i.toString() + "'>" +
+                      "$(function () {" +
+                        "$('#led" + i.toString() + "').colorpicker({" +
+                          "format: 'hex'" +
+                        "});" +
+                      "});" +
+                    "</script>";
+      $('#flex' + (i - (i % 10)).toString()).append(led_group);
+    }
+  } else if (leds < previousNumber) {
+    for (var i = previousNumber - 1; i >= leds; i--) {
+      $('#led' + i.toString()).remove();
+      $('#colorpicker' + i.toString()).remove();
+      $('#flex' + i.toString()).remove();
+    }
+  }
+  previousNumber = leds;
 });
